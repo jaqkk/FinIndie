3ef963c1f5dae18aa0be455b50601168e5f60280
jaqkk <100259593+jaqkk@users.noreply.github.com>
2022-10-05 11:51:10 -0700

    Add project files.

---
+++ Pages/TestCalc.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinIndie.Pages
+{
+	public class CalcModel : PageModel
+	{
+		private readonly ILogger<CalcModel> _logger;
+
+		public CalcModel(ILogger<CalcModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
